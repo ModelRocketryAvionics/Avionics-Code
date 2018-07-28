da2e5d70bc0a27eb5e112cbae3d5bde7ddc4be47
Michael Graves <iamnewhb@gmail.com>
2018-07-28 00:00:39 -0700

    Fixing the Repo.. Should work now?

---
+++ Debug/main.d
@@ -0,0 +1,19 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h
+main.obj: D:/ti/TivaWare_C_Series-2.1.4.178/inc/tm4c123gh6pm.h
+main.obj: D:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+main.obj: ../sch.h
+main.obj: ../ser.h
+main.obj: ../rf_cc2500.h
+
+../main.c: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h: 
+D:/ti/TivaWare_C_Series-2.1.4.178/inc/tm4c123gh6pm.h: 
+D:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+../sch.h: 
+../ser.h: 
+../rf_cc2500.h: 
